184cdcb68a197392129a34b16905cc53d1c59c42
aliciaguiradelo <alicia.guiradelo@outlook.com>
2023-03-23 20:01:47 -0300

    revisão js

---
+++ aulas/rotas/src/Componentes/Home.js
@@ -0,0 +1,16 @@
+export default function Home(){
+
+    const home={
+        backgroundColor:'#bff',
+        height: '500px',
+        textAlign: 'center',
+        color: '#000'
+    }
+
+    return(
+        <div>
+            <h1>Página home</h1>
+            <p>Esta é uma página inicial</p>
+        </div>
+    )
+}
---
+++ aulas/rotas/src/Componentes/Servicos.js
@@ -0,0 +1,16 @@
+export default function Servicos(){
+
+    const Servico={
+        backgroundColor:'#darkblue',
+        height: '500px',
+        textAlign: 'center',
+        color: '#fff'
+    }
+
+    return(
+        <div style={Servicos}>
+            <h1>Página de servicos</h1>
+            <p>Esta é uma página de servicos</p>
+        </div>
+    )
+}
